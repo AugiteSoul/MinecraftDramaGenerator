9989f01ce075b5926a9b6b8e0d11dfc450cd7c22
AugiteSoul <arthursibra@gmail.com>
2017-04-17 21:21:18 +0200

    Add files via upload

--- drama.php
+++ drama.php
@@ -6,52 +6,26 @@
 	}
 ?>
 <?php if (!isset($_GET["plain"])) : ?>
-<!DOCTYPE html>
-<html>
-<head><title>MINECRAFT DRAMA GENERATOR</title>
-<style type="text/css">
-h6 {
-	text-align: center;
-	font-weight: normal;
-	color: #777;
-}
-a {
-	color: #55C;
-}
-h3 {
-	text-align: center;
-	font-family: serif;
-	font-weight: normal;
-	font-size: 24px;
-}
-h1 {
-	text-align: center;
-	font-family: sans-serif;
-}
-</style>
-</head>
-<body>
-<h3>The Minecraft Drama Generator</h3>
-<h1><?php endif; ?>
+<?php endif; ?>
 <?php
 $combinations = array(
-	"people" => array("Player", "jadedcat", "Alblaka", "Greg", "Eloraam", "AUTOMATIC_MAIDEN", "Alz454", "RS485", "Shukaro", "Toops", "CovertJaguar", "Pahimar", "Sengir", "Azanor", "jeb", "Greymerk", "Dinnerbone", "Grum", "dan200", "Cloudy", "KingLemming", "Zeldo", "AlgorithmX2", "Mikee", "Eyamaz", "kakermix", "cpw", "LexManos", "Vswe", "Direwolf20", "Calclavia", "Reika", "Sangar", "skyboy", "FlowerChild", "SpaceToad", "ChickenBones", "Notch", "Pokefenn", "Shadowclaimer", "Vazkii", "pixlepix", "nekosune", "copygirl", "immibis", "RichardG", "JAKJ", "mDiyo", "pillbox", "progwml6", "PowerCrystals", "GUIpsp", "nallar", "Soaryn", "Soaryn", "AbrarSyed", "Sunstrike", "BevoLJ", "asie", "tterrag", "CrazyPants", "Aidan", "Binnie", "Mojang", "ProfMobius", "peterix", "RWTema", "Slowpoke", "Curse", "bspkrs", "Mr_okushama", "Searge", "iChun", "Krapht", "Erasmus_Crowley", "MysteriousAges", "Drullkus", "Micdoodle8","GenPage","Hunterz","tgame14", "Velotican", "kirindave", "MachineMuse", "M3gaFr3ak", "Lunatrius", "wha-ha-ha"),
-	"sites" => array("FTB Forums", "MCF", "Reddit", "4chan", "Technic Forums", "IC2 Forums", "GitHub", "BitBucket", "IRC", "ForgeCraft", "Patreon", "BTW Forums", "GregTech thread", "Google+", "Twitch"),
-	"things" => array("ForgeCraft", "Simply Jetpacks", "RedPower 2", "ModLoader", "RedLogic", "Forge MultiPart", "Project: Red", "BuildCraft", "Tinkers' Steelworks", "Artifice", "Roguelike Dungeons", "IndustrialCraft 2", "Equivalent Exchange", "Forestry", "RailCraft", "Simple Jetpacks", "Compact Solars", "ComputerCraft", "Wireless Redstone", "OpenComputers", "GregTech", "Ars Magica", "Thaumcraft", "FTB", "Technic", "Resonant Rise", "MineFactory Reloaded", "Magic Farm 2", "Tekkit", "MCPC+", "ATLauncher", "Metallurgy", "Logistics Pipes", "MCUpdater", "MultiMC", "Curse", "Mojang", "Test Pack Please Ignore", "Agrarian Skies", "Steve's Carts", "Steve's Factory Manager", "BiblioCraft",  "Minecraft", "XyCraft", "Forge", "GregTech", "OpenBlocks", "OpenPeripheral", "OpenComputers", "MFFS", "RotaryCraft", "Big Reactors", "Thermal Expansion 3", "Extra Utilities", "Universal Electricity", "Not Enough Items", "Portal Gun mod", "the Mojang launcher", "Too Many Items", "OptiFine", "Extra Cells", "ExtraBiomesXL", "Biomes O' Plenty", "Better than Wolves", "Schematica", "Tinker's Construct", "Natura", "Hexxit", "Iron Chests", "open-source mods", "closed-source mods", "Not Enough Mods", "Ender IO", "Mekanism", "Minecraft 1.7", "Pixelmon", "Pixelmon", "JABBA", "WAILA", "Opis", "CraftGuide", "Iguana Tweaks", "Tinkers Mechworks", "the Minecraft Drama Generator", "MineChem", "LittleMaidMob", "MCP", "Immibis' Microblocks", "Carpenter's Blocks", "Chisel", "Applied Energistics", "Applied Energistics 2", "Rotatable Blocks", "EnhancedPortals 3", "Ex Nihilo", "Ex Aliquo", "Magic Bees", "BetterStorage", "Backpacks", "Aether II", "Highlands", "Alternate Terrain Generation", "InfiCraft", "Bukkit", "Spigot", "SpoutCraft", "MortTech", "ICBM", "Galacticraft", "Modular Power Suits", "Team CoFH", "Extra Bees", "Extra Trees", "Mo' Creatures", "Grimoire of Gaia", "Atum", "Agriculture", "Sync", "Hats", "Nether Ores"),
-	"packs" => array("Feed The Beast", "the ForgeCraft pack", "FTB Monster", "FTB Unstable", "Agrarian Skies", "Direwolf20 Pack", "Tekkit", "Hexxit", "ATLauncher", "Resonant Rise", "MCUpdater", "Attack of the B-Team", "Mindcrack", "Magic Maiden", "ForgeCraft", "Technic"),
-	"function" => array("MJ support", "RF support", "EU support", "FMP compatibility", "quarries", "automatic mining", "GregTech balance", "ComputerCraft APIs", "OpenComputers APIs", "Bukkit plugin compatibility", "MCPC+ support", "ID allocation", "ore processing", "smelting", "crafting", "balance", "bees", "ThaumCraft integration", "realism", "decorative blocks", "new mobs", "TCon tool parts", "new wood types", "bundled cable support", "new player capes", "more drama", "less drama", "microblocks", "drama generation commands", "Blutricity support", "overpowered items", "underpowered items", "new ores", "better SMP support", "achievements", "quests", "more annoying worldgen"),
+	"people" => array("augitesoul", "vos", "rory", "Vamig Aliev", "BlesseNTumble", "Excrafter", "AlexSocol", "AndreySV", "Byomakuta", "Agentx49", "Thaun_", "KingOfMiners", "pra", "RonFall", "SteveKung", "MJRLegends", "micdoodle8", "radfast", "EzerArch"),
+	"sites" => array("something rnadom", "Venom Gaming", "GC Forums", "BetterDiscord", "BetterDiscord2", "augitesite", "Google", "angusnicneven.com", "Wikipedia", "VK", "MCForums"),
+	"things" => array("Galacticraft 3", "Galacticraft 4", "Galacticraft 2", "Amun-Ra", "More Planets", "More Planets 2", "Extra Planets", "GalaxySpace", "Galaxy Additions", "Space_Advanced", "Blank Planet", "Extended Planets", "Vacuum Horizon", "4Space"),
+	"packs" => array("Feed The Beast", "the ForgeCraft pack", "FTB Monster", "FTB Unstable", "Agrarian Skies", "Direwolf20 Pack", "Tekkit", "Hexxit", "ATLauncher", "Resonant Rise", "MCUpdater", "Attack of the B-Team", "Mindcrack", "Dreamland", "ForgeCraft", "Technic"),
+	"function" => array("MJ support", "RF support", "EU support", "FMP compatibility", "tiered rockets", "automatic mining", "GregTech balance", "ComputerCraft APIs", "OpenComputers APIs", "Bukkit plugin compatibility", "MCPC+ support", "ID allocation", "ore processing", "smelting", "crafting", "balance", "bees", "ThaumCraft integration", "realism", "decorative blocks", "new mobs", "TCon tool parts", "new wood types", "bundled cable support", "new player capes", "more drama", "less drama", "microblocks", "drama generation commands", "Blutricity support", "overpowered items", "underpowered items", "new ores", "better SMP support", "achievements", "quests", "more annoying worldgen", "new fuels", "evolved mobs", "gases", "dyed stuff", "dungeons"),
 	"adj" => array("bad", "wrong", "illegal", "horrible", "nasty", "not in ForgeCraft", "noncompliant with Mojang's EULA", "a serious problem", "incompatible", "a waste of time", "wonderful", "amazing", "toxic", "too vanilla", "shameful", "disappointing", "bloated", "outdated", "incorrect", "full of drama", "too realistic"),
-	"badsoft" => array("malware", "spyware", "adware", "DRM", "viruses", "trojans", "keyloggers", "stolen code", "easter eggs", "potential login stealers", "adf.ly links", "bad code", "stolen assets", "malicious code", "secret backdoors"),
-	"drama" => array("bugs", "crashes", "drama", "lots of drama", "imbalance", "pain and suffering", "piracy", "bees", "adf.ly"),
-	"crash" => array("crash", "explode", "break", "lag", "blow up", "corrupt chunks", "corrupt worlds", "rain hellfish", "spawn bees"),
-	"ban" => array("ban", "kick", "put a pumpkin of shame on", "add items mocking", "blacklist", "whitelist", "give admin rights to", "shame", "destroy"),
-	"code" => array("code", "assets", "ideas", "concepts", "a single function", "5 lines of code", "a class", "a few files", "a ZIP file", "Gradle buildscripts", "a GitHub repository"),
-	"worse" => array("worse", "better", "faster", "slower", "more stable", "less buggy"),
-	"ac1" => array("sue", "destroy the life of", "flame", "cause drama about", "complain about", "kick"),
-	"price" => array("200$", "250$", "300$", "350$", "400$", "450$", "500$", "600$"),
-	"activates" => array("activates", "works", "functions", "breaks"),
-	"says" => array("says", "tweets", "claims", "confirms", "denies"),
-	"enormous" => array("big", "large", "huge", "gigantic", "enormous")
+	"badsoft" => array("malware", "spyware", "adware", "DRM", "viruses", "trojans", "keyloggers", "stolen code", "easter eggs", "potential login stealers", "adf.ly links", "bad code", "stolen assets", "malicious code", "secret backdoors", "ransomware", "CoS", "Internet Explorer", "hidden commands", "arbitrary code execution"),
+	"drama" => array("bugs", "crashes", "drama", "lots of drama", "imbalance", "pain and suffering", "piracy", "bees", "adf.ly", "no more cookies", "unsealed environement"),
+	"crash" => array("crash", "explode", "break", "lag", "blow up", "corrupt chunks", "corrupt worlds", "rain hellfish", "spawn bees", "make wrong dimension IDs"),
+	"ban" => array("ban", "kick", "put a pumpkin of shame on", "add items mocking", "blacklist", "whitelist", "give admin rights to", "shame", "destroy", "kill"),
+	"code" => array("code", "assets", "ideas", "concepts", "a single function", "5 lines of code", "a class", "a few files", "a ZIP file", "Gradle buildscripts", "a GitHub repository", "a variable", "a binary image", "hex"),
+	"worse" => array("worse", "better", "faster", "slower", "more stable", "less buggy", "weaker", "stronger", "harder", "greenier"),
+	"ac1" => array("sue", "destroy the life of", "flame", "cause drama about", "complain about", "kick", "ban", "kill", "annoy", "steal all the cookies of"),
+	"price" => array("200$", "250$", "300$", "350$", "400$", "450$", "500$", "600$", "0.1$"),
+	"activates" => array("activates", "works", "functions", "breaks", "starts", "executes"),
+	"says" => array("says", "tweets", "claims", "confirms", "denies", "whispers", "shouts", "announces", "prints", "posts", "spreads the word", "writes"),
+	"enormous" => array("big", "large", "huge", "gigantic", "enormous", )
 );
 $sentences = array(
 	"[people] launched a DoS attack on the website of [things]",
@@ -171,10 +145,4 @@ foreach(array_keys($combinations) as $key) {
 echo($s);
 ?>
 <?php if (!isset($_GET["plain"])) : ?>
-</h1>
-<h3><a href="http://asie.pl/drama.php">Give it one more try!</a></h3>
-<h6>Over <?php echo $drama_inc+file_get_contents("/srv/http/drama.txt"); ?> dramas and counting!<br><br>
-Now a Minecraft mod by Mr_okushama! <a href="https://t.co/oc26NfyxKF">1.6.4</a> | <a href="http://t.co/lFTrmuHzVr">1.7.2</a></h6>
-</body>
-</html>
 <?php endif; ?>
