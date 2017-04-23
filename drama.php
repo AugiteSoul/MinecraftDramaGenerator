36c8ea1fd0115abb73931d291403e056bc3e7493
AugiteSoul <arthursibra@gmail.com>
2017-04-23 16:24:14 +0200

    heading for final

---
+++ drama.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+
+
+
+<head>
+	<link href="favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+	<link href="favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" /><!-- for IE -->
+	<link href="favicon.ico" rel="icon" type="image/x-icon" />
+	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<title>Mighty Drama Generator</title>
+	<style type="text/css">body { 
+    background-image: url('backBlock.png');
+    background-repeat: repeat;
+    background-attachment: fixed;
+	background-size: 8%;}
+	
+p {
+	text-align: center;
+	font-family: sans-serif;
+	color: white;
+	border: 1px black;}
+	
+	h1 {
+    font-family:arial, sans-serif;
+    font-size:100%;
+    color:#696969;
+    background-color:black;
+    text-align:center;}
+	
+	</style>
+	
+</head>
+
+<audio autoplay>
+    <source src="boom.mp3" type="audio/mpeg" >
+   </audio>
+
+<body>
+<p style="text-align: center;"><span style="font-size:26px;"><strong>AugiteSoul&#39;s Galacticraft Drama Generator!</strong></span></p>
+
+<h1><?php include 'drama_source.php';?><h1>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">https://github.com/AugiteSoul/MinecraftDramaGenerator</p>
+
+<p>&nbsp;</p>
+</body>
+</html>
